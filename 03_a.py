c81b733e833da3bba4ed4691843c02776f5bcbd2
Maik Glatki <maik@glatki.eu>
2021-12-07 19:40:05 +0100

    2021-12-03b

--- 03_a.py
+++ 03_a.py
@@ -1,28 +1,37 @@
 #! /usr/bin/env python3
 import aocd
-from math import pow
 
 data = aocd.get_data(year=2021, day=3).splitlines()
 
-bit_count = []
-for i in range(len(data[0])):
-    bit_count += [0]
+# test_set = ["011","010","100"]
 
-for point in data:
-    for bit in range(len(bit_count)):
-        if point[bit] == "1":
-            bit_count[bit] += 1
+def rate_set(numbers, bit = None, rate_oxy = True):
+    if bit == None:
+        bit = 0
+    ones = []
+    zeroes = []
+    for number in numbers:
+        if number[bit] == "1":
+           ones += [number]
         else:
-            bit_count[bit] -= 1
+           zeroes += [number]
 
-gamma = 0
-epsilon = 0
-for bit in range(len(bit_count)):
-    if bit_count[bit] > 0:
-        gamma += pow(2, len(bit_count) - bit - 1)
-    else: # lazy, but maybe it helps for the next puzzle
-        epsilon += pow(2, len(bit_count) - bit - 1)
+    if rate_oxy: # take bigger set, ore ones
+        if len(ones) >= len(zeroes):
+            keep = ones
+        else:
+            keep = zeroes
+    else: # take smaller set, or zeroes
+        if len(ones) < len(zeroes):
+            keep = ones
+        else:
+            keep = zeroes
+    if len(keep) == 1:
+        return int(keep[0],2)
+    else:
+        return rate_set(keep, bit + 1, rate_oxy)
 
-print(f"gamma = {int(gamma)}, epsilon = {int(epsilon)}")
-result = int(gamma * epsilon)
-aocd.submit(result, part="a", year=2021, day=3)
+oxygen = rate_set(data)
+co2 = rate_set(data, rate_oxy = False)
+print(f"oxygen: {oxygen}, co2: {co2}, life_support: {co2 * oxygen}")
+aocd.submit(co2 * oxygen, part="b", year=2021, day=3)
